e8179befa707404a070a09589e96911d565fdc5f
suman <suman.kunwar@daliaresearch.com>
2019-02-08 17:21:37 +0100

    added custom routes

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/me": "/users/1",
+    "/me/points": "/points/1",
+    "/me/rewards": "/rewards"
+}))
 const router = jsonServer.router('app.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4000;
